89d5a327c6b3b2219a03d06c93841985ed37d18b
Sean <sean.connolly.boutin@gmail.com>
2021-07-19 10:23:24 -0400

    Completed the double choke problem Added more functions to compressible flow solver. A detailed description of the functions to come in a README soon.

--- CompressibleFlowFunctions.py
+++ CompressibleFlowFunctions.py
@@ -16,7 +16,7 @@ def mass_from_area(Po,To,Rs,gamma,Dpipe): ##Function calculates the mass flow ra
 
 def mach_from_pressure_ratio(Po1,Po2,gamma):
     ##For a desired stagnation pressure ratio, this function calculates the Mach number before a NSW
-    tol  = 1e-5 #Tolerance for convergence method
+    tol  = 1e-9 #Tolerance for convergence method
     Mi   = 20 #First guess for Mach number
     Por  = Po2/Po1 ##Desired pressure ratio
     Prat = (((gamma+1)*Mi*Mi)/((gamma-1)*Mi*Mi+2))**(gamma/(gamma-1))*((gamma+1)/(2*gamma*Mi*Mi-(gamma-1)))**(1/(gamma-1)) ##Solution from the NSW stagnation pressure ratio equation.
@@ -35,6 +35,21 @@ def mach_from_pressure_ratio(Po1,Po2,gamma):
             M = (a+b)/2
     return M
 
+def mach_after_shock(M1,gamma):
+    ##Calculates the Mach number after a NSW knowing the pre-shock Mach number
+    M2 = np.sqrt(((gamma-1)*M1*M1+2)/(2*gamma*M1*M1-(gamma-1)))
+    return M2
+
+def pstatic_after_shock(M,gamma,P):
+    ##Calculates the static pressure after a NSW knowing the pre-choke Mach number, gamma, and static pressure
+    P2 = P*(2*gamma*M*M-(gamma-1))/(gamma+1)
+    return P2
+
+def pstag_after_shock(M,gamma,Po1):
+    ##Calculates the stagnation pressure after a NSW knowing the pre-choke Mach number, gamma
+    Po2 = Po1*(((gamma+1)*M*M)/((gamma-1)*M*M+2))**(gamma/(gamma-1))*((gamma+1)/(2*gamma*M*M-(gamma-1)))**(1/(gamma-1)) ##Solution from the NSW stagnation pressure ratio equation.
+    return Po2
+
 def astar_all_else_known(Dpipe,M,gamma):
     ##Function calculates the choking area using the compressible area ratio knowing all other properties
     Dpipe  = Dpipe*0.0254
@@ -43,6 +58,55 @@ def astar_all_else_known(Dpipe,M,gamma):
     Astar  = Apipe/Aratio
     Dstar  = np.sqrt(Astar*4/np.pi)
     return Astar, Dstar
+
+def mach_from_aratio_subsonic(Aexit,Astar,gamma):
+    ##Function calculates the choking area using the compressible area ratio knowing all other properties
+    tol     = 1e-9 #Tolerance for convergence method
+    Mguess  = 0.99
+    Apipe   = Aexit
+    Aratio1 = Apipe/Astar
+    Aratio2 = ((gamma+1)/2)**(-(gamma+1)/(2*(gamma-1)))*(1+(gamma-1)/2*Mguess*Mguess)**((gamma+1)/(2*(gamma-1)))/Mguess
+    zero    = Aratio1 - Aratio2
+    a = 0
+    b = Mguess
+    M = (a + b)/2
+    while abs(zero) > tol:
+        Aratio = ((gamma+1)/2)**(-(gamma+1)/(2*(gamma-1)))*(1+(gamma-1)/2*M*M)**((gamma+1)/(2*(gamma-1)))/M
+        zero   = Aratio1 - Aratio
+        if zero < 0:
+            a = M
+            M = (a+b)/2
+        elif zero > 0:
+            b = M
+            M = (a+b)/2
+    return M
+
+def mach_from_aratio_supersonic(Aexit,Astar,gamma):
+    ##Function calculates the choking area using the compressible area ratio knowing all other properties
+    tol     = 1e-9 #Tolerance for convergence method
+    Mguess  = 100
+    Aratio1 = Aexit/Astar
+    Aratio2 = ((gamma+1)/2)**(-(gamma+1)/(2*(gamma-1)))*(1+(gamma-1)/2*Mguess*Mguess)**((gamma+1)/(2*(gamma-1)))/Mguess
+    zero    = Aratio1 - Aratio2
+    a = 1
+    b = Mguess
+    M = Mguess
+    while abs(zero) > tol:
+        Aratio = ((gamma+1)/2)**(-(gamma+1)/(2*(gamma-1)))*(1+(gamma-1)/2*M*M)**((gamma+1)/(2*(gamma-1)))/M
+        zero   = Aratio1 - Aratio
+        if zero > 0:
+            a = M
+            M = (a+b)/2
+        elif zero < 0:
+            b = M
+            M = (a+b)/2
+    return M
+
+def p_from_pratio(Po,gamma,M):
+    ##Function calculates the static pressure knowing the gas properties, Mach number, and stagnation pressure using the isentropic pressure ratio equation P/Po
+    Po = Po*101325/14.7
+    P_static = Po*(1+((gamma-1)/2)*M**2)**(-(gamma)/(gamma-1))
+    return P_static
 
 def hole_numbers(Dhole,Astar):
     Dhole = Dhole*0.0254
--- DoubleChokeProblem.py
+++ DoubleChokeProblem.py
@@ -5,7 +5,7 @@ from CompressibleFlowFunctions import *
 
 ##Calculate the injection area required to choke the injector, and the needle valve effective diameter with the following flow values:
 Po1        = 800 #psi
-Po2        = 200 #psi
+Po2        = 400 #psi
 To         = 300 #Kelvin
 Dopipe     = 3/8 #pipe outer diameter in inches
 PipeT      = 0.049 #wall thickness in inches
@@ -19,24 +19,57 @@ Y_ox       = 96/124
 Y_fuel     = 1-Y_ox
 Dhole      = 1/32 #inches
 
-## Part 1: Calculate the injector area knowing the stagnation conditions at state 2
+##Preliminary calculations
+mdot_ox   = mdot_t*Y_ox/1000
+mdot_fuel = mdot_t*Y_fuel/1000
+
+Dpipe = Dopipe-2*PipeT
+Dpipe   = Dpipe*0.0254
+Apipe   = np.pi*Dpipe**2/4
+
+
+## Part 1 ##: Calculate the injector area knowing the stagnation conditions at state 2
 Rs_ox      = Ru/Mm_ox
 Rs_fuel    = Ru/Mm_fuel
-Astar_ox   = area_from_mass(Po2, To, Rs_ox, gamma_ox, mdot_t*Y_ox/1000)
-Astar_fuel = area_from_mass(Po2, To, Rs_fuel, gamma_fuel, mdot_t*Y_fuel/1000)
+Astar_ox   = area_from_mass(Po2, To, Rs_ox, gamma_ox, mdot_ox)
+Astar_fuel = area_from_mass(Po2, To, Rs_fuel, gamma_fuel, mdot_fuel)
 
-## Part 1.1: How many should each injector have knowing the hole size is 1/32"?
+## Part 1.1 ##: How many should each injector have knowing the hole size is 1/32"?
 numholes_ox   = hole_numbers(Dhole,Astar_ox)
-numholes_fuel = hole_numbers(Dhole,Astar_fuel)
+numholes_fuel = hole_numbers(Dhole*2,Astar_fuel)
 
-##Part 2: Assuming there is a NSW at the exit of the needle valve, what needle valve area and effective diameter allow this?
+
+## Part 2 ##: Assuming the back pressure Pb = Po2, and knowing the mass flow rate of each gas, determine the location of the NSW in the nozzle.
+##This section was solved step by step, this is why some variables overwrite each other
+
 Dpipe = Dopipe-2*PipeT
+Aneedle_ox   = area_from_mass(Po1,To,Rs_ox,gamma_ox,mdot_ox)
+Aneedle_fuel = area_from_mass(Po1,To,Rs_fuel,gamma_fuel,mdot_fuel)
 
+#Consider the subsonic branch
+M_ox   = mach_from_aratio_subsonic(Apipe,Aneedle_ox,gamma_ox)
+M_fuel = mach_from_aratio_subsonic(Apipe,Aneedle_fuel,gamma_fuel)
+P_ox   = p_from_pratio(Po1,gamma_ox,M_ox)
+P_fuel = p_from_pratio(Po1,gamma_fuel,M_fuel)
+#This branch produces static pressures much larger than Po2 which we consider to be the back pressure Pb, therefore the flow must have gone supersonic at some point
+#Consider the supersonic branch
+M_ox   = mach_from_aratio_supersonic(Apipe,Aneedle_ox,gamma_ox)
+M_fuel = mach_from_aratio_supersonic(Apipe,Aneedle_fuel,gamma_fuel)
+P_ox   = p_from_pratio(Po1,gamma_ox,M_ox)
+P_fuel = p_from_pratio(Po1,gamma_fuel,M_fuel)
+#The supersonic branch produces static pressure much smaller than Pb, therefore we consider the appearance of a NSW
+M2_ox   = mach_after_shock(M_ox,gamma_ox)
+M2_fuel = mach_after_shock(M_fuel,gamma_fuel)
+P2_ox   = pstatic_after_shock(M_ox,gamma_ox,P_ox)
+P2_fuel = pstatic_after_shock(M_fuel,gamma_fuel,P_fuel)
+#We notice Pb is between the supersonic and subsonic solutions. This indicates the presence of a NSW somewhere in the diverging part of the nozzle
+#Now, we must find the area ratio where the shock can exist
+#we know Po1 and Po2, so we find the Mach number ahead of the shock:
 M_ox         = mach_from_pressure_ratio(Po1, Po2, gamma_ox)
-Aneedle_ox, Dneedle_ox   = astar_all_else_known(Dpipe,M_ox,gamma_ox)
+M_fuel       = mach_from_pressure_ratio(Po1, Po2, gamma_fuel)\
+#Now that we know what the Mach number ahead of the shock is, we can find the cross sectional area using:
+Aratio_ox   = ((gamma_ox+1)/2)**(-(gamma_ox+1)/(2*(gamma_ox-1)))*(1+(gamma_ox-1)/2*M_ox*M_ox)**((gamma_ox+1)/(2*(gamma_ox-1)))/M_ox
+Aratio_fuel = ((gamma_fuel+1)/2)**(-(gamma_fuel+1)/(2*(gamma_fuel-1)))*(1+(gamma_fuel-1)/2*M_fuel*M_fuel)**((gamma_fuel+1)/(2*(gamma_fuel-1)))/M_fuel
 
-M_fuel       = mach_from_pressure_ratio(Po1, Po2, gamma_fuel)
-Aneedle_fuel, Dneedle_fuel = astar_all_else_known(Dpipe,M_fuel,gamma_fuel)
-
-Dneedle_ox = Dneedle_ox/0.0254
-Dneedle_fuel = Dneedle_fuel/0.0254
+Ashock_ox   = Astar_ox*Aratio_ox
+Ashock_fuel = Astar_fuel*Aratio_fuel
