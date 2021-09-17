6537096aedc7ae92668ffdca9e5c9ca95f5ec698
seanfcb <sean.connolly.boutin@gmail.com>
2021-09-17 13:16:20 -0400

    -Simple changes to reflect unit changes and function modifications in CompressibleFlowFunctions.py

--- fanno_flow.py
+++ fanno_flow.py
@@ -7,11 +7,11 @@ from scipy.optimize import bisect
 Po_ox     = 800*101325/14.7 #psi
 Po_fuel   = 800*101325/14.7
 To         = 300 #Kelvin
-Dopipe     = 1/2 #pipe outer diameter in inches
+Dopipe     = 3/8 #pipe outer diameter in inches
 PipeT      = 0.049 #wall thickness in inches
 gamma_ox   = 1.4
 gamma_fuel = 1.24
-mdot_t     = 400 #g/s
+mdot_t     = 200 #g/s
 Mm_ox      = 32 #molar mass
 Mm_fuel    = 28 #molar mass
 Ru         = 8314.5
@@ -31,8 +31,9 @@ Dpipe = Dopipe-2*PipeT
 Dpipe   = Dpipe*0.0254
 Apipe   = np.pi*Dpipe**2/4
 
-## Step 1: Calculate the Darcy friction factor using the Colebrook-White equation and assuming Re = inf
-darcy = (-2*np.log10(epsilon/(3.7*Dpipe)))**(-2)
+## Step 1: Calculate the Fanning friction factor using the
+darcy   = (-2*np.log10(epsilon/(3.7*Dpipe)))**(-2) #Darcy factor from Colebrook-White equation and assuming Re = inf
+fanning = darcy/4 #Darcy = 4*Fanning
 
 ## Step 2: Calculate Astar from flow properties
 Astar_ox   = area_from_mass(Po_ox,To,Rs_ox,gamma_ox,mdot_ox)
@@ -46,9 +47,22 @@ Mi_fuel = mach_from_aratio(Apipe,Astar_fuel,gamma_fuel,'subsonic')
 Pi_ox   = p_from_pratio(Po_ox,gamma_ox,Mi_ox)
 Pi_fuel = p_from_pratio(Po_fuel,gamma_fuel,Mi_fuel)
 
+## Step 5: Calculating Reynolds numer and fanning friction factor
+Ti_ox     = T_from_Tratio(To,gamma_ox,Mi_ox)
+Ti_fuel   = T_from_Tratio(To,gamma_fuel,Mi_fuel)
+rhoi_ox   = Pi_ox/(Ti_ox*Rs_ox)
+rhoi_fuel = Pi_fuel/(Ti_fuel*Rs_fuel)
+Re_ox     = rhoi_ox*Mi_ox*np.sqrt(gamma_ox*Rs_ox*Ti_ox)*Dpipe/12e-6
+Re_fuel   = rhoi_fuel*Mi_fuel*np.sqrt(gamma_fuel*Rs_fuel*Ti_fuel)*Dpipe/90e-6
+
+def colebrook_white(f,Re,D,epsilon):
+    return 1/np.sqrt(f) - (-2)*np.log10(epsilon/3.7*D + 2.51/(Re*np.sqrt(f)))
+darcy = bisect(colebrook_white,1e-6,1,args=(Re_ox,Dpipe,epsilon))
+fanning = darcy/4
+
 ## Step 5: Using the equation for friction flow, calcualte the length Lstar to choke the pipe
 def fanno(Lstar,f,D,M,gamma): #Define the Fanno equation to iterate on
     return (1-M**2)/(gamma*M**2) + (gamma+1)/(2*gamma)*np.log(((gamma+1)*M**2)/(2*(1+(gamma-1)/2*M**2))) - 4*f*Lstar/D
 
-Lstar_ox   = bisect(fanno,0,1e6,args=(darcy,Dpipe,Mi_ox,gamma_ox))
-Lstar_fuel = bisect(fanno,0,1e6,args=(darcy,Dpipe,Mi_fuel,gamma_fuel))
+Lstar_ox   = bisect(fanno,0,1e6,args=(fanning,Dpipe,Mi_ox,gamma_ox))
+Lstar_fuel = bisect(fanno,0,1e6,args=(fanning,Dpipe,Mi_fuel,gamma_fuel))
