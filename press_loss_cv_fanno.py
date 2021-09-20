3a4b87ba6127c45d7404e91f6fa2e96d0a14743f
seanfcb <sean.connolly.boutin@gmail.com>
2021-09-20 12:42:51 -0400

    Clearer, more frequent print statements to debug unphysical pressure increase through valve -Print statements including location (e.g. Before solenoid valve) -Print statements now print P_static, Po, M at each location -Values rounded to 2 decimal places for pressures, 4 for Mach numbers to make easier to read.

--- press_loss_cv_fanno.py
+++ press_loss_cv_fanno.py
@@ -35,12 +35,15 @@ Poratf  = (1/M_initial)*((2+(gamma-1)*M_initial**2)/(gamma+1))**((gamma+1)/(2*ga
 Postar  = Po1_initial/Poratf
 Po_bval = Postar*(1/M)*((2+(gamma-1)*M**2)/(gamma+1))**((gamma+1)/(2*gamma-2))
 P1      = p_from_pratio(Po_bval,gamma,M)
+print("Sequence:","P_static","P_total","Mach number")
+print("Before green valve",round(P1,2),round(Po_bval,2),round(M,4))
 
 #Static and Stagnation Pressure drop through green valve
 P1 = bisect(flowrates, 0, P1,args=(P1,Cv_green,SG,Q))
-print(P1)
 M_aval  = bisect(delta_mass,0.0001,0.99,args=(mdot,P1*101325/14.7,Rs,To,gamma,Apipe))
 Po_aval = P1*(1+((gamma-1)/2)*M_aval**2)**((gamma)/(gamma-1))
+print("After green valve",round(P1,2),round(Po_aval,2),round(M_aval,4))
+
 
 #Friction losses between green and black valve
 Po1_metric = Po_aval*101325/14.7
@@ -60,12 +63,13 @@ Poratf  = (1/M_initial)*((2+(gamma-1)*M_initial**2)/(gamma+1))**((gamma+1)/(2*ga
 Postar  = Po1_initial/Poratf
 Po_bval = Postar*(1/M)*((2+(gamma-1)*M**2)/(gamma+1))**((gamma+1)/(2*gamma-2))
 P1      = p_from_pratio(Po_bval,gamma,M)
+print("Before black valve",round(P1,2),round(Po_bval,2),round(M,4))
 
 #Pressure drop through black valve
 P1 = bisect(flowrates, 0.001, P1,args=(P1,Cv_black,SG,Q))
-print(P1)
 M_aval  = bisect(delta_mass,0.0001,0.99,args=(mdot,P1*101325/14.7,Rs,To,gamma,Apipe))
 Po_aval = P1*(1+((gamma-1)/2)*M_aval**2)**((gamma)/(gamma-1))
+print("After black valve",round(P1,2),round(Po_aval,2),round(M_aval,4))
 
 
 #Friction losses between black and solenoid valve
@@ -86,12 +90,13 @@ Poratf  = (1/M_initial)*((2+(gamma-1)*M_initial**2)/(gamma+1))**((gamma+1)/(2*ga
 Postar  = Po1_initial/Poratf
 Po_bval = Postar*(1/M)*((2+(gamma-1)*M**2)/(gamma+1))**((gamma+1)/(2*gamma-2))
 P1      = p_from_pratio(Po_bval,gamma,M)
+print("Before solenoid valve",round(P1,2),round(Po_bval,2),round(M,4))
 
 #Pressure drop through solenoid valve
 P1 = bisect(flowrates, 0.001, P1,args=(P1,Cv_solen,SG,Q))
-print(P1)
 M_aval  = bisect(delta_mass,0.0001,0.99,args=(mdot,P1*101325/14.7,Rs,To,gamma,Apipe))
 Po_aval = P1*(1+((gamma-1)/2)*M_aval**2)**((gamma)/(gamma-1))
+print("After solenoid valve",round(P1,2),round(Po_aval,2),round(M_aval,4))
 
 #Friction losses between solenoid and needle valve
 Po1_metric = Po_aval*101325/14.7
@@ -111,12 +116,13 @@ Poratf  = (1/M_initial)*((2+(gamma-1)*M_initial**2)/(gamma+1))**((gamma+1)/(2*ga
 Postar  = Po1_initial/Poratf
 Po_bval = Postar*(1/M)*((2+(gamma-1)*M**2)/(gamma+1))**((gamma+1)/(2*gamma-2))
 P1      = p_from_pratio(Po_bval,gamma,M)
+print("Before needle valve",round(P1,2),round(Po_bval,2),round(M,4))
 
 #Pressure drop through needle valve
 P1 = bisect(flowrates, 0.001, P1,args=(P1,Cv_nvalv,SG,Q))
-print(P1)
 M_aval  = bisect(delta_mass,0.0001,0.99,args=(mdot,P1*101325/14.7,Rs,To,gamma,Apipe))
 Po_aval = P1*(1+((gamma-1)/2)*M_aval**2)**((gamma)/(gamma-1))
+print("After needle valve",round(P1,2),round(Po_aval,2),round(M_aval,4))
 
 #Friction losses between needle and check valve
 Po1_metric = Po_aval*101325/14.7
@@ -136,22 +142,24 @@ Poratf  = (1/M_initial)*((2+(gamma-1)*M_initial**2)/(gamma+1))**((gamma+1)/(2*ga
 Postar  = Po1_initial/Poratf
 Po_bval = Postar*(1/M)*((2+(gamma-1)*M**2)/(gamma+1))**((gamma+1)/(2*gamma-2))
 P1      = p_from_pratio(Po_bval,gamma,M)
+print("Before check valve",round(P1,2),round(Po_bval,2),round(M,4))
 
 
 #Pressure drop through check valve
 P1 = bisect(flowrates, 0.001, P1,args=(P1,Cv_check,SG,Q))
-print(P1)
 
 def delta_mass(M,mdot,P,Rs,To,gamma,A):
     return mdot/1000 - P*(1+(gamma-1)/2*M*M)**(gamma/(gamma-1))*A*np.sqrt(gamma/(Rs*To))*M*(1+(gamma-1)/2*M*M)**(-(gamma+1)/(2*(gamma-1)))
 
 Mf  = bisect(delta_mass,0.0001,0.99,args=(mdot,P1*101325/14.7,Rs,To,gamma,Apipe))
 Pof = P1*(1+((gamma-1)/2)*Mf**2)**((gamma)/(gamma-1))
-print("The static pressure after the check valve is {} psi".format(P1))
-print("The Mach number after the check valve is {} ".format(Mf))
-print("The stagnation pressure after the check valve is {} psi".format(Pof))
+print("After needle valve",round(P1,2),round(Pof,2),round(Mf,4))
 
-#Calculating the effective area\
-m_star = mass_from_area(Mf,Pof*101325/14.7,To,Rs,gamma,Apipe)*1000
-Aeff = area_from_mass(Pof*101325/14.7,To,Rs,gamma,m_star/1000) #assuming a choked line
-print("The effective area of the oxygen line is {} sq. mm at {} g/s".format(Aeff*1e6,mdot))
+# print("The static pressure after the check valve is {} psi".format(P1))
+# print("The Mach number after the check valve is {} ".format(Mf))
+# print("The stagnation pressure after the check valve is {} psi".format(Pof))
+
+# #Calculating the effective area\
+# m_star = mass_from_area(Mf,Pof*101325/14.7,To,Rs,gamma,Apipe)*1000
+# Aeff = area_from_mass(Pof*101325/14.7,To,Rs,gamma,m_star/1000) #assuming a choked line
+# print("The effective area of the oxygen line is {} sq. mm at {} g/s".format(Aeff*1e6,mdot))
