c18f623d82a1a2c043ddec626cf584d4f8f7b588
seanfcb <sean.connolly.boutin@gmail.com>
2021-11-19 14:08:26 -0500

    backwayds_fanno.py script now iterates to find max mdot -Next step: iterate on Cv of needle valve to plot mdot vs Cv -Minor changes to fannocvloss_algo.py. Script now runs off the initial state of Ethylene

--- backwards_fanno.py
+++ backwards_fanno.py
@@ -44,100 +44,105 @@ M2  = 1
 ##==================================================================##
 ## Calculate conditions at exit of check valve
 ##==================================================================##
-#def back_fanno(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange):
-mdot = bisect(delta_mass, 0.0001, 1000000, args=(M2,Po2*101325/14.7,Rs,To,gamma,Apipe))
-Q =  mdot_to_scfh(mdot,Rs,SG)
+def back_fanno(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange):
+    print_statements = []
 
-M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_thru_flange)
+    mdot = bisect(delta_mass, 0.0001, 1000000, args=(M2,Po2*101325/14.7,Rs,To,gamma,Apipe))
+    Q =  mdot_to_scfh(mdot,Rs,SG)
 
-tabular_print("Before engine",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
-tabular_print("After check valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
+    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_thru_flange)
 
-##==================================================================##
-## Calculate conditions upstream of check valve
-##==================================================================##
-P2, Po2, M2 = valve_losses_backwards(P1,Cv_check,SG,Q,mdot,Rs,To,gamma,Apipe)
-f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
-Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
-tabular_print("Before check valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
+    tabular_print("Before engine",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
+    tabular_print("After check valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
 
-##==================================================================##
-## Calculate conditions downstream of run valve
-##==================================================================##
+    ##==================================================================##
+    ## Calculate conditions upstream of check valve
+    ##==================================================================##
+    P2, Po2, M2 = valve_losses_backwards(P1,Cv_check,SG,Q,mdot,Rs,To,gamma,Apipe)
+    f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
+    Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
+    tabular_print("Before check valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
 
-M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_check)
-tabular_print("After run valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
-##==================================================================##
+    ##==================================================================##
+    ## Calculate conditions downstream of run valve
+    ##==================================================================##
 
-##==================================================================##
-## Calculate conditions upstream of run valve
-##==================================================================##
-P2, Po2, M2 = valve_losses_backwards(P1,Cv_ballv,SG,Q,mdot,Rs,To,gamma,Apipe)
-f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
-Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
-tabular_print("Before run valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
+    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_check)
+    tabular_print("After run valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
+    ##==================================================================##
 
-##==================================================================##
-## Calculate conditions downstream of needle valve
-##==================================================================##
+    ##==================================================================##
+    ## Calculate conditions upstream of run valve
+    ##==================================================================##
+    P2, Po2, M2 = valve_losses_backwards(P1,Cv_ballv,SG,Q,mdot,Rs,To,gamma,Apipe)
+    f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
+    Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
+    tabular_print("Before run valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
 
-M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_bval3)
-tabular_print("After needle valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
-##==================================================================##
+    ##==================================================================##
+    ## Calculate conditions downstream of needle valve
+    ##==================================================================##
 
-##==================================================================##
-## Calculate conditions upstream of needle valve
-##==================================================================##
-P2, Po2, M2 = valve_losses_backwards(P1,Cv_nvalv,SG,Q,mdot,Rs,To,gamma,Apipe)
-f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
-Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
-tabular_print("Before needle valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
+    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_bval3)
+    tabular_print("After needle valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
+    ##==================================================================##
 
-##==================================================================##
-## Calculate conditions downstream of manual interlock valve
-##==================================================================##
+    ##==================================================================##
+    ## Calculate conditions upstream of needle valve
+    ##==================================================================##
+    P2, Po2, M2 = valve_losses_backwards(P1,Cv_nvalv,SG,Q,mdot,Rs,To,gamma,Apipe)
+    f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
+    Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
+    tabular_print("Before needle valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
 
-M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_needle)
-tabular_print("After manual interlock valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
+    ##==================================================================##
+    ## Calculate conditions downstream of manual interlock valve
+    ##==================================================================##
 
-##==================================================================##
-## Calculate conditions upstream of manual interlock valve
-##==================================================================##
-P2, Po2, M2 = valve_losses_backwards(P1,Cv_ballv,SG,Q,mdot,Rs,To,gamma,Apipe)
-f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
-Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
-tabular_print("Before manual interlock  valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
+    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_needle)
+    tabular_print("After manual interlock valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
 
-##==================================================================##
-## Calculate conditions downstream of bottle valve
-##==================================================================##
+    ##==================================================================##
+    ## Calculate conditions upstream of manual interlock valve
+    ##==================================================================##
+    P2, Po2, M2 = valve_losses_backwards(P1,Cv_ballv,SG,Q,mdot,Rs,To,gamma,Apipe)
+    f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
+    Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
+    tabular_print("Before manual interlock  valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
 
-M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_bval2)
-tabular_print("After bottle valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
+    ##==================================================================##
+    ## Calculate conditions downstream of bottle valve
+    ##==================================================================##
 
-##==================================================================##
-## Calculate conditions upstream of bottle valve
-##==================================================================##
-P2, Po2, M2 = valve_losses_backwards(P1,Cv_ballv,SG,Q,mdot,Rs,To,gamma,Apipe)
-f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
-Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
-tabular_print("Before bottle valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
+    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_bval2)
+    tabular_print("After bottle valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
 
-##==================================================================##
-## Calculate inlet conditions
-##==================================================================##
+    ##==================================================================##
+    ## Calculate conditions upstream of bottle valve
+    ##==================================================================##
+    P2, Po2, M2 = valve_losses_backwards(P1,Cv_ballv,SG,Q,mdot,Rs,To,gamma,Apipe)
+    f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon)
+    Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
+    tabular_print("Before bottle valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
 
-M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_bval1)
-tabular_print("Bottle valve inlet conditions",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
+    ##==================================================================##
+    ## Calculate inlet conditions
+    ##==================================================================##
+
+    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_bval1)
+    tabular_print("Bottle valve inlet conditions",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
 
 
 
-tabular_print("Location","P_static","P_total","Mach number","Lstar") #set headers
-tabular_print("Mass Flow Rate",mdot,"g/s") #set headers
+    tabular_print("Location","P_static","P_total","Mach number","Lstar") #set headers
+    tabular_print("Mass Flow Rate",mdot,"g/s") #set headers
 
-print(tabulate(reversed(print_statements)))
+    #print(tabulate(reversed(print_statements)))
+    print_statements = []
+    #print(tabulate(print_statements))
+    return Po1-Po1_initial
+#Pbottle = back_fanno(Po1_initial,Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange)
 
-#     return Po1-Po1_initial
 #
-#
-# Pbottle = bisect(back_fanno,14.7,Po1_initial*10,args=(Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange))
+Pexit = bisect(back_fanno,14.7,Po1_initial,args=(Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange))
+print(tabulate(print_statements))
