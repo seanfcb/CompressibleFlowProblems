c157eb13779d2c1f724cfa7acd4a3d4f33fcb8ac
seanfcb <38043177+seanfcb@users.noreply.github.com>
2021-12-06 14:53:46 -0500

    Minor modification to specs array
    Specifying fuel and oxidizer no longer necessary.

--- fannocvloss_algo.py
+++ fannocvloss_algo.py
@@ -1,25 +1,27 @@
 ######  Input file for flow_system_pressure_loss.py
+import numpy as np
 
-##Quantities specific to Oxygen (O2)
-Mm         = 32 #Molar Mass of oxygen
-gamma      = 1.4 #gamma of oxygen
-To         = 300
-Rs         = 8314.5/Mm # calculate the specific gas constant
-SG         = 1.1044 #specific gravity of O2 vs Air
-mdot       = 150 #grams per second
-mu         = 12e-6
-gammarat   = (gamma+1)/(gamma-1)
-
-
-# ##Quantities specific to Ethylene (C2H4)
-# Mm         = 28 #Molar Mass of ethylene
-# gamma      = 1.24 #gamma of ethylene
+# ##Quantities specific to Oxygen (O2)
+# Mm         = 32 #Molar Mass of oxygen
+# gamma      = 1.4 #gamma of oxygen
 # To         = 300
 # Rs         = 8314.5/Mm # calculate the specific gas constant
-# SG         = 0.9686 #specific gravity of C2H4 vs Air
-# #mdot       = 155 #grams per second
-# mu         = 90e-6
+# SG         = 1.1044 #specific gravity of O2 vs Air
+# mdot       = 150 #grams per second
+# mu         = 12e-6
 # gammarat   = (gamma+1)/(gamma-1)
+# specs      = [2.862626*0.0254,30*np.pi/180] #Format for spacer_sizing.[dia,alpha_inj]
+
+##Quantities specific to Ethylene (C2H4)
+Mm         = 28 #Molar Mass of ethylene
+gamma      = 1.24 #gamma of ethylene
+To         = 300
+Rs         = 8314.5/Mm # calculate the specific gas constant
+SG         = 0.9686 #specific gravity of C2H4 vs Air
+#mdot       = 155 #grams per second
+mu         = 90e-6
+gammarat   = (gamma+1)/(gamma-1)
+specs      = [1.892*0.0254,30*np.pi/180] #Format for spacer_sizing.[dia,alpha_inj]
 
 
 # ##Quantities specific to Hydrogen (H2)
@@ -31,6 +33,7 @@ gammarat   = (gamma+1)/(gamma-1)
 # mdot       = 155 #grams per second
 # mu         = TBD
 # gammarat   = (gamma+1)/(gamma-1)
+# specs      = [1.892*0.0254,30*np.pi/180] #Format for spacer_sizing.[dia,alpha_inj]
 
 
 
