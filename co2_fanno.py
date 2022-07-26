4a9aeadc0b23b0cd2946c3bdd43beee3e7e27525
seanfcb <sean.connolly.boutin@gmail.com>
2022-07-26 10:24:35 -0400

    Finished CO2 calculations. Added time display to print statements.

--- co2_fanno.py
+++ co2_fanno.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 from scipy.optimize import bisect
 from CompressibleFlowFunctions.algos import *
 from CompressibleFlowFunctions.Isentropic import *
@@ -54,31 +55,27 @@ M2  = 1
 ##==================================================================##
 ## Calculate conditions at exit of check valve
 ##==================================================================##
-def back_fanno(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,L_to_bval2,fluid):
+def back_fanno(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_solen,L_hose,fluid):
     print_statements = []
     Po2_init = Po2
     mdot = bisect(delta_mass, 0.0001, 1000000, args=(M2,Po2*101325/14.7,Rs,To,gamma,Apipe))
     Q =  mdot_to_scfh(mdot,Rs,SG)
-    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_thru_flange,fluid)
-
-    tabular_print("Before engine",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
-    tabular_print("After check valve",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
-
+    P1 = p_from_pratio(Po2,gamma,1)
     ##==================================================================##
     ## Calculate conditions upstream of solenoid valve
     ##==================================================================##
-    P2, Po2, M2 = valve_losses_backwards(P1,Cv_check,SG,Q,mdot,Rs,To,gamma,Apipe)
+    P2, Po2, M2 = valve_losses_backwards(P1,Cv_solen,SG,Q,mdot,Rs,To,gamma,Apipe)
     f, Re       = fanning_and_reynolds(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,fluid)
     Lstar2      =  Lstar_fanno(f,Dpipe,M2,gamma)
-    tabular_print("Before check valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
+    tabular_print("Before solenoid valve",round(P2,2),round(Po2,2),round(M2,4),Lstar2)
 
 
     ##==================================================================##
     ## Calculate inlet conditions
     ##==================================================================##
 
-    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_to_bval1,fluid)
-    tabular_print("Bottle valve inlet conditions",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
+    M1, Po1, P1, Po2, P2, Lstar1, Lstar2 = fanno_losses_backwards(Po2,To,gamma,M2,Rs,Dpipe,mu,epsilon,L_hose,fluid)
+    tabular_print("Bottle regulator exit",round(P1,2),round(Po1,2),round(M1,4),Lstar1)
 
 
     tabular_print("Location","P_static","P_total","Mach number","Lstar") #set headers
@@ -87,25 +84,24 @@ def back_fanno(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, S
     #print(tabulate(reversed(print_statements)))
     print_statements = []
     #print(tabulate(print_statements))
-    return Po1, mdot, nvalv_rat
-def fanno_iterator(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, fluid, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange):
-    Po1, mdot, nvalv_rat = back_fanno(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange,fluid)
+    return Po1, mdot
+def fanno_iterator(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, fluid, SG,Cv_solen,L_hose):
+    Po1, mdot = back_fanno(Po2, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_solen,L_hose,fluid)
     return Po1-Po1_initial
 #Pbottle = back_fanno(Po1_initial,Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_nvalv,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange)
 
 #
 Cv_min           = 0.1
-Cv_solen        = [1.2]#np.linspace(Cv_min,Cv_nvalv,round((Cv_nvalv-Cv_min)/0.1)+1)#[1,2,3,4,5,6,7,8,9]
+Cv_solen        = 1.2#np.linspace(Cv_min,Cv_nvalv,round((Cv_nvalv-Cv_min)/0.1)+1)#[1,2,3,4,5,6,7,8,9]
 result           = []
 result_nohead    = []
 
-result.append(["Cv_needle","mdot (g/s)", "Pratio, needle valve","Pbottle (psi)","Pexit (psi)","mdot/Pexit (bare with me)","Spacer thickness (thou)"])
-for x in Cv_solen:
-    Pexit = bisect(fanno_iterator,0.1,Po1_initial,args=(Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, fluid, SG,Cv_ballv,x,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange))
-    Pbottle, mdot,nvalv_rat = back_fanno(Pexit, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,x,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange,fluid)
-    t = newton(spacer_sizing,10,args=(Pexit*101325/14.7,To,Rs,mdot,gamma,specs))/0.0254*1000
-    result_nohead.append([x,mdot,nvalv_rat,Pbottle,Pexit])
-    result.append([x,mdot,nvalv_rat,Pbottle,Pexit,mdot/Pexit,t])
+result.append(["Cv_solen","mdot (g/s)","Pbottle (psi)","Pexit (psi)"])
+Pexit = bisect(fanno_iterator,0.1,Po1_initial,args=(Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, fluid, SG,Cv_solen,L_hose))
+Pbottle, mdot = back_fanno(Pexit, Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_solen,L_hose,fluid)
+# t = newton(spacer_sizing,10,args=(Pexit*101325/14.7,To,Rs,mdot,gamma,specs))/0.0254*1000
+result_nohead.append([Cv_solen,mdot,Pbottle,Pexit])
+result.append([Cv_solen,mdot,Pbottle,Pexit])
 
 
 # Pexit = bisect(fanno_iterator,0.1,Po1_initial,args=(Po1_initial, M2, Rs, To, gamma, Apipe, Dpipe, mu, epsilon, SG,Cv_ballv,Cv_needle,Cv_check,L_to_bval1,L_to_bval2,L_to_needle,L_to_bval3,L_to_check,L_thru_flange))
@@ -116,18 +112,25 @@ for x in Cv_solen:
 print(tabulate(print_statements))
 print(tabulate(result))
 
-Cv_sol   = column(result_nohead,0)
-pressrat = column(result_nohead,2)
-dotm     = column(result_nohead,1)
+m = 101325*0.53/(Rs*To)
 
-figure, axis = plt.subplots(1,2)
+seconds = m/(mdot/1000) #Time in seconds to fill sandworm
+print("Time to fill sandworm with CO2 (min): ")
+time = str(datetime.timedelta(seconds=seconds))
+print(time)
 
-axis[0].plot(Cv_n,pressrat)
-axis[0].set_xlabel("Cv_needle valve")
-axis[0].set_ylabel("P2/P1")
-
-axis[1].plot(Cv_n,dotm)
-axis[1].set_xlabel("Cv_needle valve")
-axis[1].set_ylabel("mdot (g/s)")
-
-plt.show()
+# Cv_solen   = column(result_nohead,0)
+# pressrat = column(result_nohead,2)
+# dotm     = column(result_nohead,1)
+#
+# figure, axis = plt.subplots(1,2)
+#
+# axis[0].plot(Cv_n,pressrat)
+# axis[0].set_xlabel("Cv_needle valve")
+# axis[0].set_ylabel("P2/P1")
+#
+# axis[1].plot(Cv_n,dotm)
+# axis[1].set_xlabel("Cv_needle valve")
+# axis[1].set_ylabel("mdot (g/s)")
+#
+# plt.show()
--- fannocvloss_algo_co2.py
+++ fannocvloss_algo_co2.py
@@ -38,22 +38,25 @@ specs      = [2.862626*0.0254,30*np.pi/180] #Format for spacer_sizing.[dia,alpha
 # gammarat   = (gamma+1)/(gamma-1)
 # specs      = [1.892*0.0254,30*np.pi/180] #Format for spacer_sizing.[dia,alpha_inj]
 
+# ##Quantities specific to Hydrogen (H2)
+# fluid      = 'co2'
+# Mm         = 44 #Molar Mass of hydrogen
+# gamma      = 1.28 #gamma of hydrogen
+# To         = 300
+# Rs         = 8314.5/Mm # calculate the specific gas constant
+# SG         = 1.5189 #specific gravity of H2 vs Air
+# mdot       = 155 #grams per second
+# mu         = TBD
+# gammarat   = (gamma+1)/(gamma-1)
+# specs      = [1.892*0.0254,30*np.pi/180] #Format for spacer_sizing.[dia,alpha_inj]
 
 
 ## Flow system specific quantities
-epsilon     = 0.25e-6
-Dopipe      = 3/4 #inches
-PipeT       = 0.065 #wall thickness in inches
-Cv_ballv    = 13.6
-Cv_nvalv    = 9.6 #catalog Cv for needle valve
-Cv_check    = 4.7 #catalog Cv for check valve
-Po1_initial = 800 #Bottle pressure (psi)
+epsilon     = 0.5e-6
+Dpipe       = 11/64 #inches
+Cv_solen    = 1.4 #catalog Cv for solenoid valve
+Po1_initial = 60 #Bottle pressure (psi)
 Po1_metric  = Po1_initial*101325/14.7
 
 ## Pipe lengths (currently approx)
-L_to_bval1    = 1.5/3.281
-L_to_bval2    = 6/3.281
-L_to_needle   = 1/3.281
-L_to_bval3    = 1/3.281
-L_to_check    = 1.66667/3.281#(2/12)/3.281
-L_thru_flange = 5/3.281
+L_hose    = 10/3.281
